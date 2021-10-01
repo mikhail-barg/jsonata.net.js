3563e702102cf675a63a048aaa5c9bb7c0fafc70
mikhail-barg <mikhail.barg@gmail.com>
2021-10-01 16:24:03 +0300

    Initial commit

---
+++ src/Jsonata.Net.Js/JsonataEngine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Jint;
+using Jint.Native;
+
+namespace Jsonata.Net.Js
+{
+    public sealed class JsonataEngine
+    {
+        private const string RESOURCE_NAME = "Jsonata.Net.Js.jsonata-es5.min.js";
+
+        private readonly JsValue m_func;
+
+        public JsonataEngine()
+        {
+            Assembly assembly = typeof(JsonataEngine).Assembly;
+            string jsonata_js;
+            using (StreamReader reader = new StreamReader(assembly.GetManifestResourceStream(RESOURCE_NAME)))
+            {
+                jsonata_js = reader.ReadToEnd();
+            };
+
+            Engine engine = new Engine()
+                .Execute(jsonata_js)
+                .Execute("function main_func(query, data_json) { return JSON.stringify(jsonata(query).evaluate(JSON.parse(data_json))); };")
+            ;
+            this.m_func = engine.GetValue("main_func");
+        }
+
+        public string Execute(string query, string jsonData)
+        {
+            JsValue result = this.m_func.Invoke(query, jsonData);
+            return result.ToObject()?.ToString();
+        }
+    }
+}
---
+++ src/Jsonata.Net.TestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Jsonata.Net.Js;
+using System;
+using System.Diagnostics;
+
+namespace Jsonata.Net.TestApp
+{
+    internal sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            JsonataEngine jsonata = new JsonataEngine();
+            string result = jsonata.Execute("$.a", "{\"a\": \"b\"}");
+            Debug.Assert(result == "\"b\"");
+        }
+    }
+}
